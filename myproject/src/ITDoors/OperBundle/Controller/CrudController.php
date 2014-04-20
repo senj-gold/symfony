453dcbda99cf377e36e961a751a81e334e95ecbc
senj <senj@senj-mob>
2014-04-20 12:54:13 +0300

    4. Создал контроллер crud + (екшены, cread, read, update, delete)(скорее всего это не самый правильный способ записи роутига но для первого раза рабочий)))

--- myproject/src/ITDoors/OperBundle/Controller/CrudController.php
+++ myproject/src/ITDoors/OperBundle/Controller/CrudController.php
@@ -8,22 +8,26 @@ class CrudController
 {
      public function indexAction($name)
     {
+          return $this->render(
+        'OperBundl:Crud:index.twig.'.$_format,
+        array('name' => $name)
+    );
         return new Response('<html><body>Hello '.$name.'!</body></html>');
     }
      public function creadAction()
     {
-        return new Response('<html><body>Hello '.$name.'!</body></html>');
+        return new Response('<html><body>Создаем '.'!</body></html>');
     }
-     public function readAction($name)
+     public function readAction()
     {
-        return new Response('<html><body>Hello '.$name.'!</body></html>');
+        return new Response('<html><body>Получаем запси !</body></html>');
     }
-     public function updateAction($name)
+     public function updateAction($id)
     {
-        return new Response('<html><body>Hello '.$name.'!</body></html>');
+        return new Response('<html><body>Обновить запись '.(int)$id.'!</body></html>');
     }
-     public function deleteAction($name)
+     public function deleteAction($id)
     {
-        return new Response('<html><body>Hello '.$name.'!</body></html>');
+        return new Response('<html><body>Удалить запись '.(int)$id.'!</body></html>');
     }
 }
