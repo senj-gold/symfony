1d292bc865c6dd3cd21a58de9c0c0239949b6a8c
senj <senj@senj-mob>
2014-04-20 16:51:21 +0300

    В контролере CRUD екшен read добавил выборку из базы всех записей.

--- myproject/src/ITDoors/OperBundle/Controller/CrudController.php
+++ myproject/src/ITDoors/OperBundle/Controller/CrudController.php
@@ -2,30 +2,33 @@
 
 namespace ITDoors\OperBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ITDoors\OperBundle\Entity\Department;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 use Symfony\Component\HttpFoundation\Response;
 
-class CrudController
+class CrudController  extends Controller
 {
      public function indexAction($name)
     {
-          return $this->render(
-        'OperBundl:Crud:index.twig.'.$_format,
-        array('name' => $name)
-    );
+//          return $this->render(
+//        'OperBundl:Crud:index.twig.'.$_format,
+//        array('name' => $name)
+//    );
         return new Response('<html><body>Hello '.$name.'!</body></html>');
     }
      public function creadAction()
     {
-//         $name = 'sdf';
-//          return $this->render(
-//                'ITDoors:OperBundle:crud:cread.twig.html',
-//                array('name' => $name)
-//         );
-          
-        return new Response('<html><body>Создаем '.'!</body></html>');
+
+        return new Response('<html><body>Создаем '.  '!</body></html>');
     }
      public function readAction()
     {
+         $em = $this->get('doctrine')->getEntityManager();
+        $posts = $em->getRepository('ITDoorsOperBundle:Department')->findAll();
+        var_dump($posts);
+        
         return new Response('<html><body>Получаем запси !</body></html>');
     }
      public function updateAction($id)
